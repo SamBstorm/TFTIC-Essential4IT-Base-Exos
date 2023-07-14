b77d2ba0eaad4a07b5c3f2c11b269d20b4cb703e
Samuel Legrain <samuel.legrain@bstorm.be>
2023-07-14 16:57:49 +0200

    Ajoutez des fichiers projet.

---
+++ Exo01-Inversion/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exo01_Inversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration des variables : je prépare la mémoire
+            int a, b, temp;
+
+            //Initialisation des variables : mes variables ont des valeurs
+            a = 5;
+            b = 7;
+
+            //Affichage de controle
+            Console.WriteLine("La valeur de a est {0}, et la valeur de b est {1}", a, b);
+
+            //Inversion
+            temp = a;
+            a = b;
+            b = temp;
+
+            //Affichage de controle
+            Console.WriteLine("La valeur de a est {0}", a);
+            Console.WriteLine("La valeur de b est {0}", b);
+        }
+    }
+}
---
+++ Exo02-ConvertisseurSecondes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exo02_ConvertisseurSecondes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temps, jours, heures, minutes, secondes;
+            Console.WriteLine("Veuillez renseigner le nombre de secondes totales :");
+            temps = int.Parse(Console.ReadLine());
+
+            secondes = temps % 60;
+            minutes = (temps / 60) % 60;
+            heures = (temps / 3600) % 24;
+            jours = temps / 86400;
+
+            //Console.WriteLine("{0} secondes donne {1} jour(s) {2} heure(s) {3} minute(s) {4} secondes",temps,jours,heures,minutes,secondes);
+
+            Console.WriteLine(
+                $"{temps} secondes donne {jours} jour(s)" +
+                $" {heures} heure(s) {minutes} minute(s)" +
+                $" {secondes} secondes");
+        }
+    }
+}
