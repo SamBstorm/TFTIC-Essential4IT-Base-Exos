3c659a3d76d0959cbde8495cfccb87c4fce24644
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-03 17:07:46 +0200

    Add Solution Ex17

---
+++ Exo17-Temperature/Program.cs
@@ -0,0 +1,14 @@
+﻿using Exo17_Temperature;
+
+Celsius celsius_gosselies;
+celsius_gosselies.temperature = 15;
+
+Fahrenheit fahrenheit_gosselies = celsius_gosselies.Convert();
+
+Fahrenheit fahrenheit_newYork;
+fahrenheit_newYork.temperature = 64;
+
+Celsius celsius_newYork = fahrenheit_newYork.Convert();
+
+Console.WriteLine($"A Gosselies il fait : {celsius_gosselies.temperature}° C ou {fahrenheit_gosselies.temperature}° F");
+Console.WriteLine($"A NewYork il fait : {celsius_newYork.temperature}° C ou {fahrenheit_newYork.temperature}° F");
---
+++ Exo17-Temperature/Structs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo17_Temperature
+{
+    public struct Celsius
+    {
+        public double temperature;
+
+        public Fahrenheit Convert()
+        {
+            Fahrenheit result;
+
+            result.temperature = temperature * 1.8 + 32;
+
+            return result;
+        }
+    }
+
+    public struct Fahrenheit
+    {
+        public double temperature;
+
+        public Celsius Convert()
+        {
+            Celsius result;
+
+            result.temperature = (temperature - 32) * (5 / 9.0);
+
+            return result;
+        }
+    }
+}
