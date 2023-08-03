2be83a02c4b7e5b242016f1204d852dca4098196
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-03 12:31:26 +0200

    Add Solution Ex16

---
+++ Ex16-TabStructure/Program.cs
@@ -0,0 +1,31 @@
+﻿using Ex16_TabStructure;
+
+const int TAILLE = 5;
+
+Point?[,] grille = new Point?[TAILLE, TAILLE];
+
+//Console.WriteLine(grille[0,0]?.ToString() ?? "null");
+
+for (int i = 0; i < TAILLE; i++)
+{
+    Point p;
+    p.X = i + 1;
+    p.Y = i + 1;
+    grille[i, i] = p;
+}
+
+for (int i = 0; i < TAILLE; i++)
+{
+    for (int j = 0; j < TAILLE; j++)
+    {
+        if (grille[i,j] is null)
+        {
+            Console.Write("\t");
+        }
+        else
+        {
+            Console.Write($"X : {((Point)grille[i,j]).X} - Y : {grille[i,j]?.Y} ");
+        }
+    }
+    Console.WriteLine();
+}
