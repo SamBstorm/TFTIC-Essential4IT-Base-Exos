d5a751ce9f63bc9912327643d4f6354670bdccdd
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-04 11:50:55 +0200

    Add Solution Ex18

---
+++ Ex18-Equation/Program.cs
@@ -0,0 +1,36 @@
+﻿using Ex18_Equation;
+
+Equation2degre eq = new Equation2degre();
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Veuillez introduire la valeur de A selon l'équation Ax² + Bx + C :");
+} while (!double.TryParse(Console.ReadLine(), out eq.A));
+
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"Veuillez introduire la valeur de B selon l'équation {eq.A}x² + Bx + C :");
+} while (!double.TryParse(Console.ReadLine(), out eq.B));
+
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"Veuillez introduire la valeur de C selon l'équation {eq.A}x² + {eq.B}x + C :");
+} while (!double.TryParse(Console.ReadLine(), out eq.C));
+
+Console.Clear();
+Console.WriteLine($"La formule est {eq.A}x² + {eq.B}x + {eq.C}");
+
+double? x1, x2;
+
+if(eq.ResoudreV3(out x1, out x2))
+{
+    Console.Write("L'équation est résolvable ");
+    if (x1 == x2) Console.WriteLine($"x vaut {x1}.");
+    else Console.WriteLine($"x peut valoir soit {x1}, soit {x2}.");
+}
+else Console.WriteLine("L'équation n'est pas résolvable...");
---
+++ Ex18-Equation/Structs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex18_Equation
+{
+    public struct Equation2degre
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public bool Resoudre(out double? X1, out double? X2)
+        {
+            bool result;
+            double delta = (B * B) - (4 * A * C);
+
+            if(delta == 0)
+            {
+                X1 = -B / (2 * A);
+                X2 = X1;
+                result = true;
+            }
+            else if(delta > 0)
+            {
+                X1 = (-B - Math.Sqrt(delta))/(2*A);
+                X2 = (-B + Math.Sqrt(delta))/(2*A);
+                result = true;
+            }
+            else
+            {
+                X1 = null;
+                X2 = null;
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool ResoudreV2(out double? X1, out double? X2)
+        {
+            double delta = (B * B) - (4 * A * C);
+            X2 = X1 = null;
+
+            if (delta < 0) return false;
+            if (delta > 0)
+            {
+                X1 = (-B - Math.Sqrt(delta)) / (2 * A);
+                X2 = (-B + Math.Sqrt(delta)) / (2 * A);
+            }
+            else
+            {
+                X2 = X1 = -B / (2 * A);
+            }
+            return true;
+        }
+
+        public bool ResoudreV3(out double? X1, out double? X2)
+        {
+            double delta = (B * B) - (4 * A * C);
+            X2 = X1 = null;
+
+            if (delta < 0) return false;
+            X1 = (-B - Math.Sqrt(delta)) / (2 * A);
+            X2 = (-B + Math.Sqrt(delta)) / (2 * A);
+            return true;
+        }
+    }
+}
