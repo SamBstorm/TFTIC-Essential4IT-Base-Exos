7daadb6145f991494d6878aa85ec8eb501d96ce7
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-04 16:14:43 +0200

    Add Solution Ex19

---
+++ Ex19-Cartes/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex19_Cartes
+{
+    public enum Couleurs { Coeur, Carreau, Pique, Trefle}
+
+    public enum Valeurs
+    {
+        Deux = 2,
+        Trois = 3,
+        Quatre = 4,
+        Cinq = 5,
+        Six = 6,
+        Sept = 7,
+        Huit = 8,
+        Neuf = 9,
+        Dix = 10,
+        Valet = 11,
+        Dame = 12,
+        Roi = 13,
+        As = 14
+    }
+}
---
+++ Ex19-Cartes/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ex19_Cartes;
+
+Carte[] paquet = new Carte[52];
+
+int i = 0;
+
+#region Méthode des slides...
+//foreach (string color in Enum.GetNames<Couleurs>())
+//{
+//    Couleurs current_color = Enum.Parse<Couleurs>(color);
+//    foreach (string value in Enum.GetNames<Valeurs>())
+//    {
+//        Valeurs current_value = Enum.Parse<Valeurs>(value);
+//        paquet[i].couleur = current_color;
+//        paquet[i].valeur = current_value;
+//        i++;
+//    }
+//} 
+#endregion
+
+#region Avec GetValues()
+
+foreach (Couleurs color in Enum.GetValues<Couleurs>())
+{
+    foreach (Valeurs value in Enum.GetValues<Valeurs>())
+    {
+        paquet[i].couleur = color;
+        paquet[i].valeur = value;
+        i++;
+    }
+}
+
+#endregion
+
+foreach (Carte carte in paquet)
+{
+    Console.WriteLine($"{carte.valeur} de {carte.couleur}");
+}
